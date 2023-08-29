9112d8bde1d7ee6d95dc6ed3906940f2e92ba99c
rahimuj570 <rahimuj570@gmail.com>
2023-08-29 23:22:27 +0600

    initialization of thymleaf

--- src/main/java/io/omni/financia/controllers/IndexController.java
+++ src/main/java/io/omni/financia/controllers/IndexController.java
@@ -1,14 +1,20 @@
 package io.omni.financia.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 @RequestMapping("/")
 public class IndexController {
     @GetMapping
-    public String index(){
-        return "Ok";
+    public String hello() {
+        return "home";
     }
 }
